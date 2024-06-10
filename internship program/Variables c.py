2e55a5c0007003ac546c845b2a5befa543d01637
Piyush Jogale <167557291+piyushjogale2601@users.noreply.github.com>
2024-06-10 09:15:25 +0530

    Delete internship program directory

--- internship program/Variables c.py
+++
@@ -1,6 +0,0 @@
-pamount=float(input("enter the principal amount:"))
-sinterest=float(input("Enter the simple interest:"))
-timne=float(input("Enter the time of period:"))
-
-interest=(pamount*sinterest*timne)/100
-print(interest)
